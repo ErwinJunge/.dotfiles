a51906a49662a3397c110f214db4d0dc5ccf158e
Erwin Junge <erwin@junge.nl>
2018-10-30 15:31:48 +0100

    Remove mypy

--- fish/functions/vmk3.fish
+++ fish/functions/vmk3.fish
@@ -1,5 +1,5 @@
 function vmk3post
-    pip install ipython pdbpp anaconda_mode autoflake flake8-bugbear flake8 pylint python-language-server pyls-isort pyls-mypy
+    pip install ipython pdbpp anaconda_mode autoflake flake8-bugbear flake8 pylint python-language-server pyls-isort
 end
 
 function vmk3
