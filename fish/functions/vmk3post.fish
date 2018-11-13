2afd784b7686f2b21bc55c430dc7dce50583035a
Erwin Junge <erwin@junge.nl>
2018-11-13 09:56:48 +0100

    Update on vmkpost

--- fish/functions/vmk3post.fish
+++ fish/functions/vmk3post.fish
@@ -1,3 +1,3 @@
 function vmk3post
-    pip install ipython pdbpp anaconda_mode autoflake flake8-bugbear flake8 pylint python-language-server pyls-isort importmagic epc
+    pip install -U ipython pdbpp anaconda_mode autoflake flake8-bugbear flake8 pylint python-language-server pyls-isort importmagic epc
 end
