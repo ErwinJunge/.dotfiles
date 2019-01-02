2642cd100ca86928a24ecbb7f72155f90cb20ab6
Erwin Junge <erwin@junge.nl>
2019-01-02 11:04:07 +0100

    add jedi for better python support

--- fish/functions/vmk2post.fish
+++ fish/functions/vmk2post.fish
@@ -1,3 +1,3 @@
 function vmk2post
-    pip install -U ipython pdbpp anaconda_mode autoflake flake8 pylint python-language-server pyls-isort importmagic epc
+    pip install -U ipython pdbpp anaconda_mode autoflake flake8 pylint python-language-server pyls-isort importmagic epc jedi
 end
