98f67e98385b9524a7b6477603b1c096619afd19
Erwin Junge <erwin@junge.nl>
2019-07-18 10:26:36 +0200

    Pin jedi until python-language-server update is released

--- fish/functions/vmk2post.fish
+++ fish/functions/vmk2post.fish
@@ -1,3 +1,3 @@
 function vmk2post
-    pip install -U ipython pdbpp autoflake flake8 pylint python-language-server pyls-isort importmagic epc jedi
+    pip install -U ipython pdbpp autoflake flake8 pylint python-language-server pyls-isort importmagic epc 'jedi<0.14'
 end
