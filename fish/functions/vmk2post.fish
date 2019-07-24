2c386291b322e3d064d463fbbdab1982e51f17aa
Erwin Junge <erwin@junge.nl>
2019-07-24 11:36:55 +0200

    Revert "Pin jedi until python-language-server update is released"
    This reverts commit 98f67e98385b9524a7b6477603b1c096619afd19.

--- fish/functions/vmk2post.fish
+++ fish/functions/vmk2post.fish
@@ -1,3 +1,3 @@
 function vmk2post
-    pip install -U ipython pdbpp autoflake flake8 pylint python-language-server pyls-isort importmagic epc 'jedi<0.14'
+    pip install -U ipython pdbpp autoflake flake8 pylint python-language-server pyls-isort importmagic epc jedi
 end
--- fish/functions/vmk3post.fish
+++ fish/functions/vmk3post.fish
@@ -1,3 +1,3 @@
 function vmk3post
-    pip install -U ipython pdbpp autoflake flake8-bugbear flake8 pylint python-language-server pyls-isort importmagic epc 'jedi<0.14'
+    pip install -U ipython pdbpp autoflake flake8-bugbear flake8 pylint python-language-server pyls-isort importmagic epc jedi
 end
