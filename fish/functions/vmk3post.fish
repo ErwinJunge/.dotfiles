7a4e3e02422b1f5d85b8d603a8493ee8e567d507
Erwin Junge <erwin@junge.nl>
2019-08-19 16:47:03 +0200

    Add rope

--- fish/functions/vmk3post.fish
+++ fish/functions/vmk3post.fish
@@ -1,3 +1,3 @@
 function vmk3post
-    pip install -U ipython pdbpp autoflake flake8-bugbear flake8 pylint python-language-server pyls-isort importmagic epc jedi
+    pip install -U ipython pdbpp autoflake flake8-bugbear flake8 pylint python-language-server pyls-isort importmagic epc jedi rope
 end
