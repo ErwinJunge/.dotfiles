b4dcaf5fc84d86d96a955b148261c17d97f83572
Erwin Junge <erwin@junge.nl>
2020-08-28 19:40:12 +0200

    Update default virtualenv packages

--- fish/functions/vmk3post.fish
+++ fish/functions/vmk3post.fish
@@ -1,4 +1,4 @@
 function vmk3post
     pip install -U pip
-    pip install -U ipython pdbpp autoflake flake8-bugbear flake8 pylint python-language-server pyls-isort importmagic epc jedi rope black
+    pip install -U ipython pdbpp autoflake flake8-bugbear flake8 pylint pylint-django python-language-server pyls-isort importmagic epc jedi rope black
 end
