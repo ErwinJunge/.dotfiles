4af269de91598ab28aeaa360c57405d649ad790a
Erwin Junge <erwin@junge.nl>
2020-12-02 09:22:56 +0100

    Use new method of activating virtualfish

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,3 @@ if not test -f ~/.config/fish/functions/fisher.fish
     curl -sLo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
     fisher
 end
-eval (/usr/bin/python -m virtualfish auto_activation compat_aliases)
